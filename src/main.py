68155b8b330e08cd327931e4ad0a9fa0c5397631
keitaro44 <157152053+keitaro44@users.noreply.github.com>
2026-09-07 19:37:51 +0300

    Initial commit: diamond inference with Keycloak and Docker

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@
 import os
 from model_utils import load_model, make_inference
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi_utils import Oauth2ClientCredentials
 from pydantic import BaseModel
+from keycloak.uma_permissions import AuthStatus
+from keycloak_utils import get_keycloak_data
+
 
 class Instance(BaseModel):
     carat: float
@@ -16,28 +19,53 @@ class Instance(BaseModel):
     y: float
     z: float
 
-app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
+app = FastAPI()
+keycloak_openid, token_endpoint = get_keycloak_data()
+oauth2_scheme = Oauth2ClientCredentials(tokenUrl=token_endpoint)
+
 model_path: str = os.getenv("MODEL_PATH")
 if model_path is None:
     raise ValueError("The environment variable $MODEL_PATH is empty!")
 
-async def is_token_correct(token: str) -> bool:
-    dummy_correct_token = "00000"
-    return token == dummy_correct_token
+
+async def get_token_status(token: str) -> AuthStatus:
+    # Проверка прав на ресурс infer_endpoint со scope doInfer
+    return keycloak_openid.has_uma_access(
+        token, "infer_endpoint#doInfer")
+
 
 async def check_token(token: str = Depends(oauth2_scheme)) -> None:
-    if not await is_token_correct(token):
+    print("---")
+    print(token)
+    print("---")
+    auth_status = await get_token_status(token)
+    is_logged = auth_status.is_logged_in
+    is_authorized = auth_status.is_authorized
+
+    print("---")
+    print(auth_status)
+    print(token_endpoint)
+    print("---")
+
+    if not is_logged:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    elif not is_authorized:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Access denied",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
 
 @app.get("/healthcheck")
 def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
+
 
 @app.post("/predictions")
 async def predictions(instance: Instance,
